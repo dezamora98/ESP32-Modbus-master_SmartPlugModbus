6e34a176dd5acaef6ee18ad7c35a13e8a882041a
Giselle Salgado <95318325+GiselleSL@users.noreply.github.com>
2023-07-22 23:05:40 -0400

    update

--- lib/charge-station-lib/ChargeController.cpp
+++ lib/charge-station-lib/ChargeController.cpp
@@ -1,13 +1,19 @@
 #include "ChargeController.h"
 
-ChargeController::ChargeController(SensorMonitorInterface *sensor, int relayPin, int id)
+ChargeController::ChargeController()
+{
+    _measurement_time = MEASUREMENT_TIME;
+}
+
+void ChargeController::init(SensorMonitorInterface *sensor, int relayPin, int id)
 {
     _sensor = sensor;
     _relayPin = relayPin;
     is_active = false;
     _id = id;
 
-    initPines(); // setup pins
+    pinMode(_relayPin, OUTPUT);
+    digitalWrite(_relayPin, LOW); // desactivar relay
 
     BaseType_t rc = xTaskCreatePinnedToCore(
         chargeControllerTask,
@@ -18,26 +24,29 @@ ChargeController::ChargeController(SensorMonitorInterface *sensor, int relayPin,
         NULL,
         ARDUINO_RUNNING_CORE);
     assert(rc == pdPASS);
-}
 
-void ChargeController::initPines()
-{
-    pinMode(_relayPin, OUTPUT);
-    digitalWrite(_relayPin, LOW); // desactivar relay
+    _measurement_timerH = xTimerCreate(
+        "PowerMeasurementTimer",
+        pdMS_TO_TICKS(_measurement_time),
+        pdTRUE,
+        this,
+        _powerMeasurementTimerCallback);
 }
 
 void ChargeController::startCharge()
 {
     digitalWrite(_relayPin, HIGH); // activar relay
     is_active = true;
-    // TODO iniciar timer para contar tiempo de carga
+    BaseType_t rc = xTimerStart(_measurement_timerH, portMAX_DELAY);
+    assert(rc == pdPASS);
 }
 
 void ChargeController::stopCharge()
 {
     digitalWrite(_relayPin, LOW); // desactivar relay
     is_active = false;
-    // TODO parar el timer
+    BaseType_t rc = xTimerStop(_measurement_timerH, portMAX_DELAY);
+    assert(rc == pdPASS);
 }
 
 bool ChargeController::getChargeState()
@@ -59,13 +68,23 @@ void ChargeController::updateData()
 
 float ChargeController::getPowerConsumption() // devuelve la cantidad de kwats que quedan
 {
-    printlnD("the watts consumption is: " + String(_wattsConsum));
-    return _wattsConsum;
+    printlnD("the watts consumption is: " + String(_powerConsumption));
+    return _powerConsumption;
 }
 
 void ChargeController::setPowerCapacity(float powerCapacity) // Cantidad de kwats a generar
 {
     _powerCapacity = powerCapacity;
+}
+
+/* ----------------------------- Timer callback ----------------------------- */
+
+void ChargeController::_powerMeasurementTimerCallback(TimerHandle_t timer_h)
+{
+    ChargeController *obj = static_cast<ChargeController *>(pvTimerGetTimerID(timer_h));
+    printD("dentro de timer");
+
+    obj->_measurement_timeout = pdTRUE;
 }
 
 /* ----------------- task function to control the charger ---------------- */
@@ -76,16 +95,23 @@ void ChargeController::chargeControllerTask(void *args)
 
     for (;;)
     {
-        wattsConsum = obj->getChargeCurrent() * 110;                                          // kWatts = A * V --- P = I*V
-        if (obj->is_active && wattsConsum < obj->getPowerConsumption() && !obj->is_connected) // si la cantidad de watts consumidos es menor que la definida
+        if (obj->_measurement_timeout)
         {
-            obj->startCharge();
-            printlnD("Start Charge");
-        }
-        else
-        {
-            obj->stopCharge();
-            printlnD("Stop Charge");
+            obj->_measurement_timeout = pdFALSE;
+            obj->_powerConsumption = obj->_sensor->read() * VOLTAGE; // Potencia consumida en el instante = corriente * voltaje 110V
+            obj->_energy += obj->_powerConsumption;                  // enegia acumulada
+
+            if (obj->is_active && obj->_energy < obj->getPowerConsumption() && obj->is_connected)
+            { // si esta activo el puerto, si la energia consumida es menor que los watts comprados y esta conectado al toma
+                obj->startCharge();
+                printlnD("Start charge");
+            }
+            else
+            {
+                obj->stopCharge();
+                printlnD("Stop charge");
+                // call updateData() para actualzar los valores en la plataforma
+            }
         }
     }
 }
